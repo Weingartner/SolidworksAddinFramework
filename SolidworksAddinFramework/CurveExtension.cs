9fb05dcccccaf57ed45e7e5721374f0076167da3
bradphelan <b.phelan@weingartner.com>
2016-02-22 16:38:37 +0100

    Add more extensions

---
+++ SolidworksAddinFramework/CurveExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SolidWorks.Interop.sldworks;
+
+namespace SolidworksAddinFramework
+{
+    public static class CurveExtension
+    {
+        public static double[] PointAt(this ICurve curve, double t)
+        {
+            return (double[]) curve.Evaluate2(t, 0);
+        }
+
+        public static double[] Domain(this ICurve curve )
+        {
+            bool isPeriodic;
+            double end;
+            bool isClosed;
+            double start;
+            curve.GetEndParams(out start, out end, out isClosed, out isPeriodic);
+            return new[] {start, end};
+
+        }
+    }
+}
---
+++ SolidworksAddinFramework/ModellerExtensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using MathNet.Numerics.LinearAlgebra.Double;
+using SolidWorks.Interop.sldworks;
+
+namespace SolidworksAddinFramework
+{
+    public static class ModellerExtensions
+    {
+        public static ICurve CreateTrimmedLine(this IModeler modeler, double[] p0, double[] p1)
+        {
+            Debug.Assert(p0.Length==p1.Length);
+            var p0v = new DenseVector(p0);
+            var p1v = new DenseVector(p1);
+            var dir = p1v - p0v;
+            var line = (ICurve)modeler.CreateLine(p0, dir.Values);
+            if(p1.Length==3)
+                return line.CreateTrimmedCurve2(p0[0], p0[1], p0[2], p1[0], p1[1], p1[2]);
+            if (p1.Length==2)
+                return line.CreateTrimmedCurve2(p0[0], p0[1], 0, p1[0], p1[1], 0);
+
+            throw new Exception("End points must have 2 or 3 elements");
+
+        }
+    }
+}
