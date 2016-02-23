4edc6ff9c1bdaa6721544f083a130cb148f73886
bradphelan <b.phelan@weingartner.com>
2016-02-23 11:32:26 +0100

    Try to use a unit vector for the direction.

--- SolidworksAddinFramework/ModellerExtensions.cs
+++ SolidworksAddinFramework/ModellerExtensions.cs
@@ -14,14 +14,23 @@ namespace SolidworksAddinFramework
             Debug.Assert(p0.Length==p1.Length);
             var p0v = new DenseVector(p0);
             var p1v = new DenseVector(p1);
-            var dir = p1v - p0v;
-            var line = (ICurve)modeler.CreateLine(p0, dir.Values);
+            var dir = (p1v - p0v).Normalize(2);
+            var line = (ICurve)modeler.CreateLine(p0, new[] {dir[0], dir[1] });
+            Debug.Assert(line != null, "line != null");
             if(p1.Length==3)
-                return line.CreateTrimmedCurve2(p0[0], p0[1], p0[2], p1[0], p1[1], p1[2]);
+            {
+                line = line.CreateTrimmedCurve2(p0[0], p0[1], p0[2], p1[0], p1[1], p1[2]);
+            }else
             if (p1.Length==2)
-                return line.CreateTrimmedCurve2(p0[0], p0[1], 0, p1[0], p1[1], 0);
+            {
+                line = line.CreateTrimmedCurve2(p0[0], p0[1], 0, p1[0], p1[1], 0);
+            }else
+                throw new Exception("End points must have 2 or 3 elements");
 
-            throw new Exception("End points must have 2 or 3 elements");
+            Debug.Assert(line != null, "line != null");
+            return line;
+
+
 
         }
     }
