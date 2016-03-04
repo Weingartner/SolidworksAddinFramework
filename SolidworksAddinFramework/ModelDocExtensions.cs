eb7df73ff8d6dd03ccf0aae2960fa026deac87f2
bradphelan <b.phelan@weingartner.com>
2016-03-04 11:00:21 +0100

    Add GetBodiesTs for ModelDocExtensions

---
+++ SolidworksAddinFramework/ModelDocExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+
+namespace SolidworksAddinFramework
+{
+    public static class ModelDocExtensions
+    {
+        public static IBody2[] GetBodiesTs(this IModelDoc2 doc, swBodyType_e type = swBodyType_e.swSolidBody,
+            bool visibleOnly = false)
+        {
+            var part = (IPartDoc) doc;
+            var objects = (object[]) part.GetBodies2((int) type, visibleOnly);
+            return objects.Cast<IBody2>().ToArray();
+        }
+    }
+}
