d65d1a685bdbc9b01ae8a428d735c114bc652385
Johannes Kronberger <Johannes.Kronberger@gmx.at>
2016-03-11 09:39:58 +0100

    Setup test environment

---
+++ SwCSharpAddinMF.Spec/SwPool.cs
@@ -0,0 +1,14 @@
+using SwCSharpAddinSpecHelper;
+using Xunit;
+
+namespace SwCSharpAddinMF.Spec
+{
+    /// <summary>
+    /// xUnit searches collection definitions only in the assembly of the test class (see https://github.com/xunit/xunit/issues/374).
+    /// So this class must be compiled in all test assemblies.
+    /// </summary>
+    [CollectionDefinition(nameof(SwPool))]
+    public class SwPool : ICollectionFixture<SwPoolFixture>
+    {
+    }
+}
