1bb0b1d1d64dd25d4e90b40a0803af58a748bf41
Johannes Kronberger <Johannes.Kronberger@gmx.at>
2016-03-14 08:58:00 +0100

    Add test project for SolidworksAddinFramework

---
+++ SolidworksAddinFramework.Spec/MathUtilitySpec.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using FluentAssertions;
+using SolidWorks.Interop.sldworks;
+using SwCSharpAddinSpecHelper;
+using Xunit;
+
+namespace SolidworksAddinFramework.Spec
+{
+    public class MathUtilitySpec : SolidWorksSpec
+    {
+        public MathUtilitySpec(SwPoolFixture pool) : base(pool)
+        {
+        }
+
+        [Fact]
+        public void ShouldBeAbleToGetCrossProduct()
+        {
+            var mathUtility = (IMathUtility) App.GetMathUtility();
+            var result = mathUtility.Cross(new [] { 1.0, 2.0, 3.0 }, new [] { 4.0, 5.0, 6.0 });
+            result.ShouldAllBeEquivalentTo(new[] { -3.0, 6.0, -3.0 });
+        }
+    }
+}
