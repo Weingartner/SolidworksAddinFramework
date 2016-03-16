530ddbdbb27157b058fa4c5114c7d113ff17ae5f
Johannes Kronberger <Johannes.Kronberger@gmx.at>
2016-03-16 13:41:50 +0100

    add vector extension

--- SolidworksAddinFramework/MathVectorExtensions.cs
+++ SolidworksAddinFramework/MathVectorExtensions.cs
@@ -1,4 +1,5 @@
-﻿using SolidWorks.Interop.sldworks;
+﻿using System;
+using SolidWorks.Interop.sldworks;
 
 namespace SolidworksAddinFramework
 {
@@ -39,5 +40,13 @@ namespace SolidworksAddinFramework
         }
 
 
+        public static double AngleBetweenVectors(this IMathVector v0, IMathVector v1)
+        {
+            if (((double[])v0.ArrayData).Length==((double[])v1.ArrayData).Length)
+            {
+                return Math.Acos(v0.Dot(v1)/(v0.GetLength()*v1.GetLength()));
+            }
+            throw new Exception("Vectors must have the same dimension!");
+        }
     }
 }
