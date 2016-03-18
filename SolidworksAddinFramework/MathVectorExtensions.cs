cd063e1137890d52fd1648d87f4544ba306753ca
Johannes Kronberger <Johannes.Kronberger@gmx.at>
2016-03-18 12:44:17 +0100

    add extension methods

--- SolidworksAddinFramework/MathVectorExtensions.cs
+++ SolidworksAddinFramework/MathVectorExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using SolidWorks.Interop.sldworks;
 
 namespace SolidworksAddinFramework
@@ -39,12 +40,18 @@ namespace SolidworksAddinFramework
             return (MathVector) v.MultiplyTransform(t);
         }
 
+        public static double LengthOfProjectionXY(this double[] vector)
+        {
+            return Math.Sqrt(vector.Take(2).Sum(c => Math.Pow(c, 2)));
+        }
 
-        public static double AngleBetweenVectors(this IMathVector v0, IMathVector v1)
+        public static double AngleBetweenVectorsSigned(this IMathVector v0, IMathVector v1)
         {
             if (((double[])v0.ArrayData).Length==((double[])v1.ArrayData).Length)
             {
-                return Math.Acos(v0.Dot(v1)/(v0.GetLength()*v1.GetLength()));
+                var sign = Math.Sign(((IMathVector) (v0.Cross(v1))).ArrayData.CastArray<double>()[2]);
+                var ret =  Math.Acos(v0.Dot(v1)/(v0.GetLength()*v1.GetLength()));
+                return sign*ret;
             }
             throw new Exception("Vectors must have the same dimension!");
         }
