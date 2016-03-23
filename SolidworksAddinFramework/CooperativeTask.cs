ef8328096fe81cdbac312f68b2a8d640aedae8bc
bradphelan <b.phelan@weingartner.com>
2016-03-23 07:37:44 +0100

    Introduce `CooperativeTask`

---
+++ SolidworksAddinFramework/CooperativeTask.cs
@@ -0,0 +1,22 @@
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SolidworksAddinFramework
+{
+    /// <summary>
+    /// Helper methods to implement cooperative tasks using coroutines (https://en.wikipedia.org/wiki/Coroutine)
+    /// </summary>
+    public class CooperativeTask
+    {
+        /// <summary>
+        /// Allows an algorithm to specify a location where it may be paused or cancelled
+        /// to allow other tasks to run or continue.
+        /// </summary>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        public static async Task Yield(CancellationToken ct)
+        {
+            await TaskEx.Delay(1, ct);
+        }
+    }
+}
