c3984127c883cfaa85ff58e70629dd3b6e1dbf88
Johannes Kronberger <Johannes.Kronberger@gmx.at>
2016-03-24 16:34:43 +0100

    add FeatureExtensions

---
+++ SolidworksAddinFramework/FeatureExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SolidWorks.Interop.sldworks;
+
+namespace SolidworksAddinFramework
+{
+    public static class FeatureExtensions
+    {
+        public static double[] GetBoxTs(this IFeature feat)
+        {
+            object boxObject = null;
+            var box = feat.GetBox(ref boxObject);
+
+            if (box)
+            {
+                return (double[])boxObject;
+            }
+            throw new Exception("Can't eveluate box!");
+        }
+    }
+}
