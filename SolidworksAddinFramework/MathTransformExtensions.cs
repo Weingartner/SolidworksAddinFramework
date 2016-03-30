b3f1f35ba2a5898fe01bd9418f5e582a545dd8c8
Johannes Kronberger <Johannes.Kronberger@gmx.at>
2016-03-30 15:16:19 +0200

    add ExtractTransform

--- SolidworksAddinFramework/MathTransformExtensions.cs
+++ SolidworksAddinFramework/MathTransformExtensions.cs
@@ -8,9 +8,17 @@ namespace SolidworksAddinFramework
     {
         public static void ExtractTransform(this IMathTransform transform, out DenseMatrix rotation, out DenseVector translation)
         {
+            double scale;
+            ExtractTransform(transform, out rotation, out translation, out scale);
+        }
+
+        public static void ExtractTransform(this IMathTransform transform, out DenseMatrix rotation, out DenseVector translation, out double scale)
+        {
             var transformArray = transform.ArrayData.CastArray<double>();
             rotation = new DenseMatrix(3, 3, transformArray.Take(9).ToArray());
             translation = new DenseVector(transformArray.Skip(9).Take(3).ToArray());
+            scale = transformArray[12];
         }
+
     }
 }
