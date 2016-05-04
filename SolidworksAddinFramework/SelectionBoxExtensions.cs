9a03f44245c06e370bb6721fcc8c5188dd474f0e
Johannes Egger <eggerjohansi@gmail.com>
2016-05-04 08:47:56 +0200

    Add extension method to set selection color of a selection box

---
+++ SolidworksAddinFramework/SelectionBoxExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+
+namespace SolidworksAddinFramework
+{
+    public static class SelectionBoxExtensions
+    {
+        public static void SetSelectionColor(this IPropertyManagerPageSelectionbox box, swUserPreferenceIntegerValue_e color)
+        {
+            box.SetSelectionColor(true, (int)color);
+        }
+    }
+}
