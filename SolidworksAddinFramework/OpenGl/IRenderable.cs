e1cf91bee7c92d1bdc58e21ed9e06bdf0ea9f636
Johannes Egger <eggerjohansi@gmail.com>
2016-05-10 11:20:08 +0200

    Add renderable Point

--- SolidworksAddinFramework/OpenGl/IRenderable.cs
+++ SolidworksAddinFramework/OpenGl/IRenderable.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Numerics;
+using OpenTK.Graphics.OpenGL;
 using SolidWorks.Interop.sldworks;
+using MGL = SolidworksAddinFramework.OpenGl.ModernOpenGl;
+using OpenTK.Graphics.OpenGL;
 
 namespace SolidworksAddinFramework.OpenGl
 {
@@ -8,5 +11,29 @@ namespace SolidworksAddinFramework.OpenGl
     {
         void Render(DateTime time);
         void ApplyTransform(Matrix4x4 transform);
+    }
+
+    public class Point : IRenderable
+    {
+        public Point(Vector3 location)
+        {
+            Location = location;
+        }
+
+        private Vector3 Location { get; }
+
+        public void Render(DateTime time)
+        {
+            using (MGL.Begin(PrimitiveType.Points))
+            {
+                Location.GLVertex3();
+            }
+
+        }
+
+        public void ApplyTransform(Matrix4x4 transform)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
