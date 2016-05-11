e6c5e8048558ad8866ee9948908647a608a08122
Johannes Egger <eggerjohansi@gmail.com>
2016-05-11 14:11:42 +0200

    Draw point as circle

--- SolidworksAddinFramework/OpenGl/Point.cs
+++ SolidworksAddinFramework/OpenGl/Point.cs
@@ -9,24 +9,45 @@ namespace SolidworksAddinFramework.OpenGl
     public class Point : IRenderable
     {
         private readonly Color _Color;
+        private readonly int _Size;
+        private bool _InFront;
 
-        public Point(Vector3 location, Color? color)
+        public Point(Vector3 location, Color color, int size, bool inFront=false)
         {
-            _Color = color ?? Color.Blue;
             Location = location;
+            _Color = color;
+            _Size = size;
+            _InFront = inFront;
         }
 
         private Vector3 Location { get; }
 
         public void Render(DateTime time)
         {
-            GL.PointSize(5);
-            using (ModernOpenGl.Begin(PrimitiveType.Points))
-            using(ModernOpenGl.SetColor(_Color, ShadingModel.Flat))
-            {
-                Location.GLVertex3();
-            }
+            //using(ModernOpenGl.SetColor(_Color, ShadingModel.Smooth))
+            //using (ModernOpenGl.Begin(PrimitiveType.Points))
+            //{
+            //    Location.GLVertex3();
+            //}
 
+            //GL.Clear(ClearBufferMask.ColorBufferBit);
+            using (ModernOpenGl.SetColor(_Color, ShadingModel.Flat))
+            {
+                GL.PointSize(_Size);
+                GL.Enable(EnableCap.AlphaTest);
+                GL.AlphaFunc(AlphaFunction.Notequal, 0);
+                GL.Enable(EnableCap.Blend);
+                GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+                GL.Enable(EnableCap.PointSmooth);
+                GL.Hint(HintTarget.PointSmoothHint, HintMode.Nicest);
+                GL.Begin(PrimitiveType.Points);
+                Location.GLVertex3();
+                GL.End();
+
+                GL.Disable(EnableCap.PointSmooth);
+                GL.BlendFunc(BlendingFactorSrc.Zero, BlendingFactorDest.Zero);
+                GL.Disable(EnableCap.Blend);
+            }
         }
 
         public void ApplyTransform(Matrix4x4 transform)
@@ -37,9 +58,10 @@ namespace SolidworksAddinFramework.OpenGl
 
     public static class PointExtensions
     {
-        public static IDisposable DisplayUndoable(this Vector3 p, IModelDoc2 doc, Color? color=null)
+        public static IDisposable DisplayUndoable(this Vector3 p, IModelDoc2 doc, Color color, int size, int layer = 0)
         {
-            return new Point(p, color).DisplayUndoable(doc);
+            return new Point(p, color, size).DisplayUndoable(doc, layer);
         }
+
     }
 }
