1798df697809b7c2796f6ea3a50782a4a2104431
bradphelan <b.phelan@weingartner.com>
2016-05-31 16:45:53 +0200

    Add method for extracting abbrevioations of strings

---
+++ SolidworksAddinFramework/StringExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SolidworksAddinFramework
+{
+    public static class StringExtensions
+    {
+        public static string Abbreviate(this string s) => new Regex(@"[^\p{Lu}]").Replace(s, "");
+    }
+}
