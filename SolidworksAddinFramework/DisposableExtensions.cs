810707cf9c3b742916ac22f8c6dd7fb8ca71fba3
bradphelan <b.phelan@weingartner.com>
2016-06-02 09:26:23 +0200

    Add helpers for scheduling idisposables

--- SolidworksAddinFramework/DisposableExtensions.cs
+++ SolidworksAddinFramework/DisposableExtensions.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Concurrency;
 using System.Reactive.Disposables;
 using System.Text;
 using System.Threading;
@@ -12,6 +14,19 @@ namespace SolidworksAddinFramework
         {
             return new CompositeDisposable(d);
         }
+        public static IDisposable Concat(this IDisposable d, IDisposable d1)
+        {
+            return new CompositeDisposable(d,d1);
+        }
+        public static IDisposable Concat(this IDisposable d, params IDisposable [] d1)
+        {
+            return new CompositeDisposable
+                (new[]
+                {
+                    d
+                }.Concat(d1));
+        }
+
 
         public static T DisposeWith<T>(this T disposable, CompositeDisposable container)
             where T : IDisposable
@@ -20,6 +35,35 @@ namespace SolidworksAddinFramework
                 container.Add(disposable);
             return disposable;
         }
+
+        public static IDisposable DisposeOn(this IDisposable d, IScheduler s )=>
+            new ScheduledDisposable(s,d);
+
         public static IDisposable DisposeWith(this IDisposable d, CancellationToken token) => token.Register(d.Dispose);
+
+
+
+
+        public static Func< IDisposable> CreateAndDisposeOn(this Func<IDisposable> fn, IScheduler s)
+        {
+            return () =>
+            {
+                var d = new SerialDisposable();
+                s.Schedule(() => d.Disposable = fn());
+                return d.DisposeOn(s);
+            };
+
+        }
+        public static Func<T0, IDisposable> CreateAndDisposeOn<T0>(this Func<T0, IDisposable> fn, IScheduler s)
+        {
+            return t0 => CreateAndDisposeOn(()=>fn(t0),s)();
+        }
+
+        public static Func<T0,T1, IDisposable> CreateAndDisposeOn<T0,T1>(this Func<T0,T1, IDisposable> fn, IScheduler s)
+        {
+            return (t0,t1) => CreateAndDisposeOn(()=>fn(t0,t1),s)();
+
+        }
+
     }
 }
