71a6b222ae85ed6e26c84f5b68a76b4287e816fd
bradphelan <b.phelan@weingartner.com>
2016-06-09 17:14:59 +0200

    Add IfTrue for fluent composition of bool

--- SolidworksAddinFramework/BoolExtensions.cs
+++ SolidworksAddinFramework/BoolExtensions.cs
@@ -11,6 +11,12 @@ namespace SolidworksAddinFramework
     public static class BoolExtensions
     {
         public static Option<T> IfTrue<T>(this bool v, Func<T> fn) => v ? Some(fn()) : None;
+        public static bool IfTrue(this bool v, Action fn)
+        {
+            if (v)
+                fn();
+            return true;
+        }
         public static Option<T> IfTrue<T>(this bool v, T t) => v.IfTrue(() => t);
     }
 }
