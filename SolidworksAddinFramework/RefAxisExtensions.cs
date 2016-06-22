53ec76c642df518c00b5bba542bda52f55b54435
bradphelan <b.phelan@weingartner.com>
2016-06-22 15:36:42 +0200

    Add method to return Edge from IRefAxis

---
+++ SolidworksAddinFramework/RefAxisExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using SolidworksAddinFramework.Geometry;
+using SolidWorks.Interop.sldworks;
+
+namespace SolidworksAddinFramework
+{
+    public static class RefAxisExtensions
+    {
+        public static Edge3 Edge(this IRefAxis axis)
+        {
+            var axisParams = axis.GetRefAxisParams().DirectCast<double[]>();
+            return new Edge3(new Vector3((float) axisParams[0], (float) axisParams[1], (float) axisParams[2])
+                ,new Vector3((float) axisParams[3], (float) axisParams[4], (float) axisParams[5]));
+        }
+    }
+}
