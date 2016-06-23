9ec2dc2666b80e83c052358f2a6e8a1486b83dea
Johannes Egger <eggerjohansi@gmail.com>
2016-06-23 07:59:57 +0200

    Add test to check equality of SelectionData

---
+++ SolidworksAddinFramework.Spec/SelectionDataSpec.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace SolidworksAddinFramework.Spec
+{
+    public class SelectionDataSpec
+    {
+        [Fact]
+        public void EqualityShouldWork()
+        {
+            var d1 = new SelectionData(new[] { new SelectionData.ObjectId(new byte[] { 1, 2, 3 }) }, 5);
+            var d2 = new SelectionData(new[] { new SelectionData.ObjectId(new byte[] { 1, 2, 3 }) }, 5);
+            d1.Equals(d2).Should().BeTrue();
+        }
+    }
+}
