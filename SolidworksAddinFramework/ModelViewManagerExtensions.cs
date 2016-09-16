153d2e3a90e35ea643dd44605d4352b33ab4cb74
Johannes Egger <eggerjohansi@gmail.com>
2016-09-16 08:31:18 +0200

    Add observable that fires when a section view is about to be created

--- SolidworksAddinFramework/ModelViewManagerExtensions.cs
+++ SolidworksAddinFramework/ModelViewManagerExtensions.cs
@@ -1,15 +1,22 @@
 using System;
 using System.Reactive.Disposables;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
 using SolidWorks.Interop.sldworks;
 
 namespace SolidworksAddinFramework
 {
     public static class ModelViewManagerExtensions
     {
+        private static readonly ISubject<SectionViewData> _CreateSectionViewObservable = new Subject<SectionViewData>();
+
+        public static IObservable<SectionViewData> CreateSectionViewObservable => _CreateSectionViewObservable.AsObservable();
+
         public static IDisposable CreateSectionView(this IModelViewManager modelViewManager, Action<SectionViewData> config)
         {
             var data = modelViewManager.CreateSectionViewData();
             config(data);
+            _CreateSectionViewObservable.OnNext(data);
             if (!modelViewManager.CreateSectionView(data))
             {
                 throw new Exception("Error while creating section view.");
