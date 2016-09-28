1177220a1f90394e799f4f5258b168c2d3b3d67f
bradphelan <b.phelan@weingartner.com>
2016-09-28 09:28:13 +0200

    Fix GenFloat to have a range generator

--- SolidworksAddinFramework.FSCheck/GenFloat.cs
+++ SolidworksAddinFramework.FSCheck/GenFloat.cs
@@ -1,4 +1,6 @@
+using System;
 using FsCheck;
+using static LanguageExt.Prelude;
 
 namespace WeinCadSW.Spec.FsCheck
 {
@@ -7,8 +9,27 @@ namespace WeinCadSW.Spec.FsCheck
     /// </summary>
     public static class GenFloat
     {
-        public static Gen<double> Normal =>
-            Arb.Default.NormalFloat().Generator.Select(f=>f.Item);
 
+        private static double fraction(int a, int b, int c) =>a + (double) b/ c;
+
+        public static Gen<double> Normal =>
+                Arb.Default.DoNotSizeInt32()
+                    .Generator
+                    .Three()
+                    .Where(v=>v.Item3.Item!=0)
+                    .Select(t=>fraction(t.Item1.Item, t.Item2.Item, t.Item3.Item));
+
+        private static Gen<double> _Range(double lower, double upper) =>
+            Normal
+            .Select(f => lower + (upper - lower)*Math.Abs(f)%1.0);
+
+        public static Gen<double> Range(double lower, double upper) =>
+            Gen.Frequency
+                (Tuple(1, Gen.Constant(lower)),
+                 Tuple(1, Gen.Constant(lower + double.Epsilon)),
+                 Tuple(1, Gen.Constant(upper - double.Epsilon)),
+                 Tuple(1, Gen.Constant(upper)),
+                 Tuple(20, GenFloat._Range(lower, upper))
+                );
     }
 }
